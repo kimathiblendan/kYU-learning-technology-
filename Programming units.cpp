310747730d1402bb02e495937d23121d67e11e44
kimathiblendan <kimathiblendan@gmail.com>
2025-09-19 16:27:05 +0300

    Add files via upload

---
+++ Programming units.cpp
@@ -0,0 +1,22 @@
+//simple c program (units 
+/*
+Name:Blendan
+reg:CT100/G/26261/25
+Description y1s1 units
+*/
+#include <stdio.h>
+
+int main()
+{
+printf("SEMESTER UNITS\n");
+printf("Analogue Electronics\n");
+printf("fundamentals of ICT\n");
+printf("programming and algorithms\n");
+printf("communication skills \n");
+printf("mathematics for science \n");
+printf("computer Systems and Organizations \n");
+printf("essentials of accounting \n");
+printf("computer applications \n");
+
+    return 0;
+}
