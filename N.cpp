2e8612f0bb351964acc3e893ef0d477221624430
kimathiblendan <kimathiblendan@gmail.com>
2025-10-01 15:09:12 +0300

    Add files via upload

---
+++ N.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float attendance, average;
+
+    printf("Enter attendance percentage: ");
+    scanf("%f", &attendance);
+
+    printf("Enter average marks: ");
+    scanf("%f", &average);
+
+    if (attendance >= 75 && average >= 40) {
+        printf("Eligible for exams.\n");
+    } else {
+        printf("Not eligible.\n");
+    }
+
+    return 0;
+}
